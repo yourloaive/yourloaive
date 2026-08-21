71e3ada0c58c68f75e54c990f96807f56a97747a
yourloaive <gg9pw29vc4@privaterelay.appleid.com>
2026-08-21 18:32:32 +0900

    update yourloaive

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const posts = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/content/posts",
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    description: z.string().optional(),
+    category: z.enum([
+      "diary",
+      "books",
+    ]),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  posts,
+};
